09ad97266ba2d88467fc2d52cb332d191a2fabfc
munyi-victor <munyivictor6@gmail.com>
2024-05-01 14:22:54 +0300

    made contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,97 @@
+"use client";
+
+import CustomButton from "@/components/CustomButton";
+
 const Contact = () => {
-  return <div className="contact h-[89vh] lg:h-[90vh]">Contact</div>;
+  const handleSubmit = () => {}
+
+  return (
+    <div className="md:h-[82vh] pt-4 w-full">
+      <h1 className="text-3xl font-semibold md:text-4xl text-center">
+        Contact us
+      </h1>
+
+      <div className="mt-6 mb-4 px-10 flex flex-col md:flex-row gap-[40px]">
+        <div className="flex-1">
+          <form className="flex flex-col space-y-4" onSubmit={handleSubmit}>
+            <div className="flex flex-col space-y-1 text-left">
+              <label
+                htmlFor="name"
+                className="text-md font-medium text-gray-700"
+              >
+                Your name:
+              </label>
+              <input
+                type="text"
+                id="name"
+                placeholder="Your name"
+                className="rounded-md border border-gray-300 px-3 py-2 focus:outline-none focus:ring-1 focus:ring-blue-500"
+              />
+            </div>
+
+            <div className="flex flex-col space-y-1 text-left">
+              <label
+                htmlFor="email"
+                className="text-md font-medium text-gray-700"
+              >
+                Your email:
+              </label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Your email"
+                className="rounded-md border border-gray-300 px-3 py-2 focus:outline-none focus:ring-1 focus:ring-blue-500"
+              />
+            </div>
+
+            <div className="flex flex-col space-y-1 text-left">
+              <label
+                htmlFor="message"
+                className="text-md font-medium text-gray-700"
+              >
+                Message:
+              </label>
+              <textarea
+                id="message"
+                rows={4}
+                placeholder="Your message"
+                className="rounded-md border border-gray-300 px-3 py-2 focus:outline-none focus:ring-1 focus:ring-blue-500"
+              />
+            </div>
+
+            <div className="flex justify-center">
+              <CustomButton
+                value="Submit"
+                customClass="secondary"
+                type="submit"
+              />
+            </div>
+          </form>
+        </div>
+
+        <div className="flex flex-col flex-1 items-center">
+          <h1 className="text-center text-2xl font-semibold">
+            Also reach us through:
+          </h1>
+          <div className="mt-4">
+            <h2 className="text-center text-xl font-semibold">Telephone:</h2>
+            <ul className="mt-2">
+              <li>+254727632463</li>
+              <li>+254708905136</li>
+            </ul>
+          </div>
+
+          <div className="mt-4">
+            <h2 className="text-center text-xl font-semibold">Social Media:</h2>
+            <ul className="mt-2 flex gap-10">
+              <li>Facebook</li>
+              <li>Instagram</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
--- components/JoinModal.tsx
+++ components/JoinModal.tsx
@@ -10,15 +10,19 @@ const JoinModal = ({closeModal}:ModalProps) => {
       role="dialog"
     >
       <div className="relative w-full max-w-2xl md:max-w-xl mx-auto shadow rounded-xl bg-white p-4 pt-0 h-fit">
-
-        <span onClick={closeModal} className="flex justify-end text-4xl cursor-pointer font-light hover:font-bold">&times;</span>
+        <span
+          onClick={closeModal}
+          className="flex justify-end text-4xl cursor-pointer font-light hover:font-bold"
+        >
+          &times;
+        </span>
 
         <form className="flex flex-col space-y-4">
           <h1 className="font-medium text-2xl text-center">Join TriomegaB</h1>
-          <div className="flex flex-col space-y-1">
+          <div className="flex flex-col space-y-1 text-left">
             <label
               htmlFor="fname"
-              className="text-sm font-medium text-gray-700 "
+              className="text-sm font-medium text-gray-700"
             >
               First Name:
             </label>
@@ -29,7 +33,7 @@ const JoinModal = ({closeModal}:ModalProps) => {
             />
           </div>
 
-          <div className="flex flex-col space-y-1">
+          <div className="flex flex-col space-y-1 text-left">
             <label
               htmlFor="lname"
               className="text-sm font-medium text-gray-700 "
@@ -43,7 +47,7 @@ const JoinModal = ({closeModal}:ModalProps) => {
             />
           </div>
 
-          <div className="flex flex-col space-y-1">
+          <div className="flex flex-col space-y-1 text-left">
             <label
               htmlFor="email"
               className="text-sm font-medium text-gray-700 "
@@ -57,7 +61,13 @@ const JoinModal = ({closeModal}:ModalProps) => {
             />
           </div>
 
-          <div className="flex justify-center"><CustomButton value="Join now" customClass="secondary" type="submit" /></div>
+          <div className="flex justify-center">
+            <CustomButton
+              value="Join now"
+              customClass="secondary"
+              type="submit"
+            />
+          </div>
         </form>
       </div>
     </div>
