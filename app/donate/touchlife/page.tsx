02d1058538453879d2687879bc2d285da29f0064
munyi-victor <munyivictor6@gmail.com>
2024-06-05 16:18:37 +0300

    modified

--- app/donate/touchlife/page.tsx
+++ app/donate/touchlife/page.tsx
@@ -1,6 +1,16 @@
 const TouchLife = () => {
   return (
-    <div className="h-[84vh] py-3">Touch a Life</div>
+    <div className="h-[84vh] py-3">
+      <div>
+        <h1>Touch a Life, Pave Blessings!</h1>
+        <p>With every kind of donations: money, clothings, etc.</p>
+        <p><i>For the hand that gives is the hand that receives..</i></p>
+      </div>
+
+      <div>
+        
+      </div>
+    </div>
   )
 }
 
